12a5526ac03ba88db8da80bea9cf2ec6b8cc5988
cfeenstra67 <cameron.l.feenstra@gmail.com>
2022-08-28 20:47:35 -0700

    tests fix

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  globals: {
+    LOG_LEVEL: 'info',
+    DEV_MODE: false,
+  },
   moduleFileExtensions: ['js', 'json', 'ts'],
   rootDir: 'test',
   testEnvironment: 'node',
